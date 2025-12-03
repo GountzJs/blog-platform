74da5ca46a060fdf2a3c74d4151d3c69ef202b2a
Gountzjs <tomasjsdev@gmail.com>
2025-12-03 18:02:45 -0300

    feat(back-blog): integrate envs and call classes entities

--- apps/back-blog/src/db/db.ts
+++ apps/back-blog/src/db/db.ts
@@ -1,15 +1,22 @@
+import { resolve } from "node:path";
+import { cwd } from "node:process";
 import { DataSource } from "typeorm";
+import { FollowEntity } from "./entities/follow.entity";
+import { ProfileEntity } from "./entities/profile.entity";
+import { UserEntity } from "./entities/user.entity";
+
+process.loadEnvFile(resolve(cwd(), ".env"));
 
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "postgres",
-	password: "postgres",
-	database: "blog_db",
+	host: process.env.DATABASE_HOST || "localhost",
+	port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+	username: process.env.DATABASE_USER || "postgres",
+	password: process.env.DATABASE_PASSWORD || "postgres",
+	database: process.env.DATABASE_NAME || "blog_db",
 	synchronize: true,
 	logging: true,
-	entities: ["./**/*.entity.ts"],
+	entities: [UserEntity, ProfileEntity, FollowEntity],
 	migrations: [],
 	subscribers: [],
 });
