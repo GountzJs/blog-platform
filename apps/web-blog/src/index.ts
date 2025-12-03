ee5df2c0bdf39c44e2c49988e3f4d0db55e6354e
Gountzjs <tomasjsdev@gmail.com>
2025-12-03 00:44:01 -0300

    feat(web-blog): add web-blog width rspack + typescript

---
+++ apps/web-blog/src/index.ts
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const rootEl = document.querySelector("#root");
+
+if (rootEl) {
+  rootEl.innerHTML = `
+  <div class="content">
+    <h1>Blog Platform</h1>
+    <p>Start building your blog with Rsbuild.</p>
+  </div>
+`;
+}
